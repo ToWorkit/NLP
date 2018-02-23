99a3aadbfda6ebc1532afbcd5fa452398dd8eeef
Toworkit <1211126374@qq.com>
2018-02-23 19:30:23 +0800

    20180223

--- python/各种实验.py
+++ python/各种实验.py
@@ -1,5 +1,20 @@
+import numpy as np
+import pandas as pd
+
 data = [1, 2, 3, 4]
 data = [i for i in data if i > 2 and i < 4]
 print(data)
 # 小数点向右移两位，并保留四位小数
 print(format(2.12345618, '.4%'))
+
+x = np.random.randn(10)
+# 各项加1
+print(x + 1)
+
+D = pd.DataFrame([x, x + 1])
+print(D)
+
+D_t = pd.DataFrame([x, x + 1]).T
+print(D_t)
+
+print(np.arange(10))
