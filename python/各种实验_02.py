4ebd06b6fbc531b76a5a6a1f6173d3bb299e9cb0
Toworkit <1211126374@qq.com>
2018-03-03 09:02:57 +0800

    20180303

--- python/各种实验_02.py
+++ python/各种实验_02.py
@@ -10,3 +10,17 @@ pd_li = pd.Series([1, 2, 3, 4, None, None, 5, None, 6])
 print(pd_li)
 # 取非空的值
 print(pd_li[pd.notnull(pd_li)])
+# 乘积
+print(pd_li[pd.notnull(pd_li)].prod(), '----')
+
+
+
+str_l = ['a', 'b', 'c', 'd', 'e']
+x = list(map(lambda i: i.split('-->'), str_l))
+print(x)
+print(type('a'.split('--')))
+
+a_ = pd.Series(['a', 'b', 'c', 'e'])
+b_ = pd.Series(['a-->b', 'a-->c', 'a-->e', 'b-->c', 'c-->e'])
+a_ = a_.append(b_)
+print(a_)
